77bb0d7469d18492d7348516bab5f174ce482e51
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-07-31 21:22:12 +0530

    admin nav data

--- app/src/main/java/ml/adityabodhankar/mobileapp/AdminPageActivity.java
+++ app/src/main/java/ml/adityabodhankar/mobileapp/AdminPageActivity.java
@@ -1,9 +1,12 @@
 package ml.adityabodhankar.mobileapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -27,14 +30,31 @@ public class AdminPageActivity extends AppCompatActivity {
         toolbar.setTitle(getString(R.string.app_name));
         setSupportActionBar(toolbar);
 
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_orders, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_orders,R.id.navigation_products,
+                R.id.navigation_category, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_admin_page);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout_btn_menu_item) {
+            CommonData.signOut(getApplicationContext());
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
