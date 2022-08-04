7fdc49498f6c242fe42cb45d5b0f5b38ac303a67
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-08-04 21:57:35 +0530

    admin product adater

---
+++ app/src/main/java/ml/adityabodhankar/mobileapp/Adapter/AdminProductAdapter.java
@@ -0,0 +1,80 @@
+package ml.adityabodhankar.mobileapp.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import ml.adityabodhankar.mobileapp.AddCategoryActivity;
+import ml.adityabodhankar.mobileapp.Models.ProductModel;
+import ml.adityabodhankar.mobileapp.R;
+
+public class AdminProductAdapter extends RecyclerView.Adapter<AdminProductAdapter.ViewHolder> {
+    Context context;
+    List<ProductModel> products;
+
+    public AdminProductAdapter(Context context, List<ProductModel> products) {
+        this.context = context;
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        return new ViewHolder(layoutInflater.inflate(R.layout.recycler_admin_product, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(products, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView image;
+        TextView title;
+        ImageButton deleteBtn;
+        private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.admin_product_image);
+            title = itemView.findViewById(R.id.admin_product_title);
+            deleteBtn = itemView.findViewById(R.id.admin_product_delete);
+        }
+
+        public void setData(List<ProductModel> products, int position) {
+            title.setText(products.get(position).getName());
+            if (!products.get(position).getImage().equalsIgnoreCase("default")){
+                Glide.with(context).load(products.get(position).getImage()).into(image);
+            }
+            deleteBtn.setOnClickListener(view -> db.collection("products").document(products.get(position).getId()).delete()
+                    .addOnSuccessListener(unused -> Toast.makeText(context, "Product Deleted.", Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(e -> Toast.makeText(context, "Product delete fail.", Toast.LENGTH_SHORT).show()));
+            itemView.setOnClickListener(view -> {
+//                Intent intent = new Intent(context, AddProductActivity.class);
+//                intent.putExtra("categoryId", products.get(position).getId());
+//                context.startActivity(intent);
+            });
+        }
+    }
+}
--- app/src/main/java/ml/adityabodhankar/mobileapp/Fragments/OrderFragment.java
+++ app/src/main/java/ml/adityabodhankar/mobileapp/Fragments/OrderFragment.java
@@ -2,65 +2,59 @@ package ml.adityabodhankar.mobileapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import ml.adityabodhankar.mobileapp.Adapter.AdminCategoryAdapter;
+import ml.adityabodhankar.mobileapp.Adapter.AdminProductAdapter;
+import ml.adityabodhankar.mobileapp.Adapter.ProductAdapter;
+import ml.adityabodhankar.mobileapp.Models.CategoryModel;
+import ml.adityabodhankar.mobileapp.Models.ProductModel;
 import ml.adityabodhankar.mobileapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OrderFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OrderFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OrderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OrderFragment newInstance(String param1, String param2) {
-        OrderFragment fragment = new OrderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FirebaseFirestore db;
+    private RecyclerView productsRecycler;
+    private ArrayList<ProductModel> products;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        db = FirebaseFirestore.getInstance();
+        productsRecycler = view.findViewById(R.id.admin_product_recycler);
+        productsRecycler.setLayoutManager(new LinearLayoutManager(requireContext()));
+        db.collection("products").addSnapshotListener((snapshots, error) -> {
+            if (snapshots != null) {
+                products = new ArrayList<>();
+                for (DocumentSnapshot snapshot : snapshots) {
+                    products.add(
+                            new ProductModel(Objects.requireNonNull(snapshot.getData())));
+                }
+                productsRecycler.setAdapter(new AdminProductAdapter(requireContext(), products));
+            }else{
+                Toast.makeText(requireContext(), "Error:- "+ Objects.requireNonNull(error).getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
